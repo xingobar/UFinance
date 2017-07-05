75a972b39b8c7acdb5588e2a7c938ee11fb52a8e
xingobar <gary840227@gmail.com>
2017-07-05 14:45:39 +0800

    fix bug

--- UFinance/resources/views/home.blade.php
+++ UFinance/resources/views/home.blade.php
@@ -9,7 +9,6 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            @foreach($transactions as $transaction)
             <div class="panel panel-default">
                 <div class="panel-heading">總交易紀錄</div>
 
@@ -17,7 +16,6 @@
                     You are logged in!
                 </div>
             </div>
-            @endforeach
         </div>
     </div>
 </div>
